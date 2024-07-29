51b2c4a7744f64c58df5fe72c610732e06ee66f8
pintoll <serendipity8271@gmail.com>
2024-07-30 00:41:00 +0900

    Feat: Tailwind Configuration
    - FSD configuration
    - prettierrc: use Tailwind Plugin
    - dependency: clsx, cva, twMerge
    - tailwind config: color theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,16 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        secondary: "#ff77b8",
+        tertiary: "#d077e5",
+      },
+    },
+  },
   plugins: [],
 };
 export default config;
